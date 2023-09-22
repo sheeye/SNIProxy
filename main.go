fe69f20d02edc370161536f4cc96b2a3149f5987
sheeye <sheeye@qq.com>
2023-09-22 19:58:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -252,19 +252,15 @@ func forward(conn net.Conn, data []byte, dst string, raddr string) {
 	}
 
 	go ioReflector(conn, backend, true, raddr, dst)
-	ioReflector(backend, conn, false, raddr, dst)
+	ioReflector(backend, conn, false, dst, raddr)
 }
 
-// ioReflector 函数接收一个 io.WriteCloser 类型的写入对象 dst、一个 io.Reader 类型的读取对象 src、一个 bool 类型的 isToClient、一个 chan int 类型的 conChk，以及两个字符串类型的 raddr 和 dsts
+// ioReflector 函数接收一个 io.WriteCloser 类型的写入对象 dst、一个 io.Reader 类型的读取对象 src、一个 bool 类型的 isToClient，以及两个字符串类型的 dstaddr 和 srcaddr
 // 该函数使用 io.Copy 函数将 src 中读取到的数据流复制到 dst 中，然后将转发的字节数写入日志
-func ioReflector(dst io.WriteCloser, src io.Reader, isToClient bool, raddr string, dsts string) {
+func ioReflector(dst io.WriteCloser, src io.Reader, isToClient bool, dstaddr string, srcaddr string) {
 	// 将 IO 流反映到另一个
 	written, _ := io.Copy(dst, src)
-	if isToClient {
-		serviceLogger(fmt.Sprintf("[%v] -> [%v] %d bytes", dsts, raddr, written), 33, true)
-	} else {
-		serviceLogger(fmt.Sprintf("[%v] -> [%v] %d bytes", raddr, dsts, written), 33, true)
-	}
+	serviceLogger(fmt.Sprintf("[%v] -> [%v] %d bytes", srcaddr, dstaddr, written), 33, true)
 }
 
 // 解析 Client Hello 消息
